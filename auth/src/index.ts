a539768446c1820c80ed6e3e4e3595b8d68519a5
Sunil Kumar <sunilpalarya@gmail.com>
2023-06-14 13:31:36 +0530

    added User model for mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handlers';
 import { NotFoundError } from './errors/not-found-error';
+
 
 
 const app = express();
@@ -21,7 +24,16 @@ app.all('*', async (req,res)=>{
 
 app.use(errorHandler);
 
-app.listen(3000, ()=>{
-    console.log('--> Auth app listening on port 3000');
-});
+const start =async ()=>{
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    } catch (error) {
+        console.error(error);
+    }
+    console.log('Connected to Mongodb')
+    app.listen(3000, ()=>{
+        console.log('--> Auth app listening on port 3000');
+    });
+};
 
+start();
